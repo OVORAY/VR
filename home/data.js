e431984683de287aec4f2c4ddd4f222b1c43aad3
OVORAY <cjxie0208@gmail.com>
2026-04-23 11:53:49 +0800

    1

---
+++ home/data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "首頁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.5350718416041822,
+        "pitch": -0.0028831379143809954,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.5466130718917945,
+          "pitch": -0.10688115436221324,
+          "title": "頂埔站4號出口",
+          "text": "臺北捷運板南線（土城線延伸頂埔段）在本站為起點站<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Taipei_MRT_Dingpu_Station_Exit_4.jpg/250px-Taipei_MRT_Dingpu_Station_Exit_4.jpg\">"
+        }
+      ]
+    }
+  ],
+  "name": "home",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ tour1/data.js
@@ -0,0 +1,270 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "永福岩清水祖師廟",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.224070884870101,
+        "pitch": 0.07462326218465876,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.377098047093714,
+          "pitch": 0.10232221884517578,
+          "rotation": 10.995574287564278,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7447405172844856,
+          "pitch": -0.4134486440247329,
+          "title": "永福岩清水祖師廟",
+          "text": "臺灣日治時期建立，建廟齡約有八十多年。奉祀福建泉州府安溪移民的守護神清水祖師。配祀觀音菩薩、關聖帝君、文昌帝君、魁星星君、朱衣神君、太歲星君等神祇。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "中央路岔路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.4265483541040496,
+        "pitch": 0.10357890952713511,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6076694186321632,
+          "pitch": 0.1909079683346988,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -2.9392238904062005,
+          "pitch": 0.2583958156946551,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.1753588210761308,
+          "pitch": 0.3725957690900792,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "頂埔國小前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.7056170467358331,
+        "pitch": 0.14581376736980545,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7334066310635432,
+          "pitch": 0.163978575659268,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        },
+        {
+          "yaw": -3.071422100375072,
+          "pitch": 0.14580494756821238,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "頂埔國小",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.7498168228164239,
+        "pitch": 0.0539380045424771,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.007361186852868,
+          "pitch": 0.19276507349528416,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "中州路岔路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.305174824579547,
+        "pitch": -0.012227994198738656,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.132267317191477,
+          "pitch": 0.2512853867432163,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.6993631613072449,
+          "pitch": 0.30262824652265863,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "公園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4055403642165345,
+        "pitch": 0.05536289728546251,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1408300652462984,
+          "pitch": 0.34110762357947344,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "祖師廟~中州路",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
