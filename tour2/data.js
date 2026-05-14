d553469cdc85cabfb4db56ba01d193dbe5f9ba2e
OVORAY <cjxie0208@gmail.com>
2026-05-14 11:26:19 +0800

    2

---
+++ tour2/data.js
@@ -0,0 +1,260 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "鴻海頂埔廠",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.299944578567527,
+        "pitch": 0.05311733522240303,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.050566918758745416,
+          "pitch": 0.1878137866692864,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2729723438475489,
+          "pitch": -0.16725089368516777,
+          "title": "Title",
+          "text": "於1974年肇基於臺灣，以模具為根基，逐漸發展為高科技服務企業。在電子代工服務領域（EMS）排名全球第一，市占率超過四成，範圍涵蓋消費性電子產品、雲端網路產品、電腦終端產品、元件及其他等四大產品領域"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "土城產業園區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.1627758874394214,
+        "pitch": 0.09002498042129758,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.320732205957972,
+          "pitch": 0.20233806472806748,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.8887114089994625,
+          "pitch": 0.15381048810840703,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6449238241497426,
+          "pitch": -0.12857721127484645,
+          "title": "土城產業園區",
+          "text": "<div>土城產業園區為一綜合性工業區，自民國61年起規劃設計，於民國64年9月完工。全區由東向西呈狹長狀目前進駐廠商共469家(105/11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "三民路岔路口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.1943772266081183,
+        "pitch": -0.27153777189544925,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.052208820619491,
+          "pitch": 0.24374141461100507,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.6671861953468721,
+          "pitch": 0.31662425800202953,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "經濟部土城產業園區服務中心",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.2854157719002295,
+        "pitch": -0.05051597375140915,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.26580561733583075,
+          "pitch": 0.15425126539147627,
+          "rotation": 10.995574287564278,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.114107530855243,
+          "pitch": 0.2851837602382208,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3338191797304528,
+          "pitch": -0.33043907587688537,
+          "title": "經濟部土城產業園區服務中心",
+          "text": "經濟部土城產業園區服務中心負責園區內廠商服務、土地管理與公共設施維護，隸屬經濟部產業園區管理局"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "消防局",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.8603919365775923,
+        "pitch": -0.29824080643858153,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.015325873425647174,
+          "pitch": 0.3445165630978444,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
